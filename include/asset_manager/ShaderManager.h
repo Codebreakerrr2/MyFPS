1457b6f8f754c223409a3873b75b3f8833eb78fb
Codebreakerrr2 <144541673+Codebreakerrr2@users.noreply.github.com>
2026-02-25 18:39:10 +0000

    implemented ShaderManager

--- include/asset_manager/ShaderManager.h
+++ include/asset_manager/ShaderManager.h
@@ -15,17 +15,24 @@ namespace Manager{
 
     private:
 
-        ShaderID nextID;
+        ShaderID nextID= 1;
         std::unordered_map<ShaderID, std::unique_ptr<Engine::Shader>> map;
         std::unordered_map<std::string,ShaderID> name_to_id;
+        std::unordered_map<ShaderID,std::string> id_to_name;
         std::vector<ShaderID> shaderRemoveMemory;
+      
+
+        bool allDestroyed = false;
+
+        
     public:
+  
         Engine::Shader* GetShader(ShaderID id);
         Engine::Shader* GetShader(const std::string& name);
         void DestroyShader(ShaderID id);
         void DestroyShader(const std::string& name);
-        Engine::Shader* CreatShader(std::string name,const std::string& pathVertex, const std::string& pathFrag);
-        void DestryAllShaders();
+        Engine::Shader* CreateShader(std::string name,const std::string& pathVertex, const std::string& pathFrag);
+        void DestroyAllShaders();
         void cleanUp();
 
     };
--- src/asset_manager/ShaderManager.cpp
+++ src/asset_manager/ShaderManager.cpp
@@ -1,3 +1,59 @@
 #include "asset_manager/ShaderManager.h"
 
+namespace Manager{
+
+ Engine::Shader* ShaderManager::GetShader(ShaderID id){
+
+    auto it = map.find(id);
+    if(it != map.end()){
+        return it->second.get();
+    }
+    return nullptr;
+ }
+
+ Engine::Shader* ShaderManager::GetShader(const std::string& name){
+    auto it = name_to_id.find(name);
+    if(it != name_to_id.end()){
+        ShaderID id = it->second; 
+        return GetShader(id);
+    }
+    return nullptr;
+    }
+
+    void ShaderManager::DestroyShader(ShaderID id){
+
+         shaderRemoveMemory.push_back(id);
+
+    }
+    void ShaderManager::DestroyShader(const std::string& name){
+       
+        auto it = name_to_id.find(name);
+        if(it != name_to_id.end()){
+            shaderRemoveMemory.push_back(it->second);
+        }
+
+    }   
+    void ShaderManager::DestroyAllShaders(){
+            allDestroyed = true;
+    }
+    void ShaderManager::cleanUp(){
+        if(allDestroyed){
+            name_to_id.clear();
+            id_to_name.clear();
+            map.clear();
+            shaderRemoveMemory.clear();
+        }
+        for(auto& rm : shaderRemoveMemory){
+            map.erase(rm);
+           auto iter = id_to_name.find(rm);
+           if(iter!=id_to_name.end()){
+            name_to_id.erase(iter->second);
+            id_to_name.erase(iter);
+           }
+        }
+        shaderRemoveMemory.clear();
+
+
+    }
+}
 
