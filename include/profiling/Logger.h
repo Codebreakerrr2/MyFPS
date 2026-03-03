4068783743c48928b6ecafd9833d5141c4a54a74
Codebreakerrr2 <144541673+Codebreakerrr2@users.noreply.github.com>
2026-03-03 23:52:08 +0000

    started working on Logging, On the Fly but needed

---
+++ include/profiling/Logger.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+
+/**
+ERROR	Kritische Fehler, die das Programm eventuell abbrechen lassen könnten
+WARNING	Probleme, die nicht kritisch sind, aber beachtet werden sollten
+INFO	Allgemeine Statusmeldungen, z. B. „Asset geladen“
+DEBUG	Detaillierte Debug-Infos für Entwickler, z. B. Variable-Werte
+TRACE	Sehr feingranulare Infos, oft pro Funktion / Schritt
+FATAL	Extreme Fehler, die sofort Programm beenden
+SUCCESS / OK	Optional, um positive Events zu loggen 
+ */
+namespace Logging{
+    
+    enum LogLevel{
+        ERROR,
+        WARNING,
+        INFO,
+        DEBUG,
+        SUCCESS
+
+    };
+
+
+class Logger{
+
+    virtual ~Logger()= default;
+
+    virtual Log(const std::string& msg,LogLevel loggerType){
+        
+    }
+
+
+
+
+};
+
+}
